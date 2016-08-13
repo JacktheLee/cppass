ff94827047839d2e19d9e2a1b2b8b0942503e6da
keunyoung kim <topluv11@gmail.com>
2016-08-13 16:53:06 +0000

    intro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813083035) do
+ActiveRecord::Schema.define(version: 20160813103810) do
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
