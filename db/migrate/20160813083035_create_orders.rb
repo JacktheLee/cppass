8c89176991f3b6a21e024c9b39339a2098fedb44
mac <hm@macui-MacBook-Pro.local>
2016-08-13 22:00:15 +0900

    form partial view making

--- db/migrate/20160813083035_create_orders.rb
+++ db/migrate/20160813083035_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      t.string        :file
+      t.boolean       :is_double_side
+      t.boolean       :is_color
+      t.integer       :slide_per_page
+      t.integer       :price
+      t.integer       :location
+      t.integer       :quantity
+      t.references    :user
       t.timestamps
     end
   end
