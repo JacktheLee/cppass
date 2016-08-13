50baec5b3bcb0d4d07e051fbf9f7f21eb057308b
미니똥미니 <ecovic94@gmail.com>
2016-08-14 00:25:55 +0900

    mypage partial tabs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping 
+  
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
   def index
+  end
+
+  def mypage
   end
 end
